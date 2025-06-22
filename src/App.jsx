8c0618c5078021c25dc40f0565abef3eb018acf8
Rishav <rishavraj4273@gmail.com>
2025-06-22 18:38:40 +0530

    feat: added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import SignIn from './pages/SignIn'
+import Profile from './pages/Profile'
+import ProtectedRoute from './components/Auth/ProtectedRoute'
 
 function App() {
 
   return (
-    <div className='px-30'>
-      <Router>
-      <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/home" element={<Home/>}/>
-      </Routes>
+    <Router>
+      <Navbar/>
+      <div className='px-30'>
+        <Routes>
+          <Route path="/" element={<Landing/>}/>
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/profile" element={<ProtectedRoute><Profile/></ProtectedRoute>}/>
+        </Routes>
+      </div>
     </Router>
-    </div>
   )
 }
 
