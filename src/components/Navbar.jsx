0e49bbea8f3090d307ed5f3d96861d92e8377c00
Rishav <rishavraj4273@gmail.com>
2025-06-24 19:05:44 +0530

    feat: added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,37 @@
 import { Scale } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import Avatar from "./Avatar";
 import Menu from "./Menu";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useClickAway } from "react-use";
-import { div } from "framer-motion/client";
 
 export default function Navbar(){
     const {user, loading} = useAuth();
     const [showDropdown, setShowDropdown] = useState(false);
+    const location = useLocation();
     const ref = useRef(null);
     
     useClickAway(ref,()=>{
         setShowDropdown(false);
     });
+    useEffect(() => {
+        setShowDropdown(false);
+    },[location])
 
     return(
         <div className="flex justify-between items-center p-4 px-30 bg-primary text-white border-b border-gray-800/50">
             <Link to={''} className="flex gap-1 items-center text-2xl font-semibold font-mono"><Scale size={24}/>LawGenie</Link>
             {user && 
-                <div>
-                    <button ref={ref} className="cursor-pointer" onClick={()=>setShowDropdown(!showDropdown)}><Avatar size={10}/></button>
-                    {showDropdown && <Menu ref={ref}/>}
+                <div ref={ref} className="relative">
+                    <div className="flex gap-4 items-center text-sm font-semibold">
+                        <button className="cursor-pointer" onClick={()=>setShowDropdown(!showDropdown)}><Avatar size={10}/></button>
+                        {showDropdown && <Menu/>}
+                    </div>
                 </div>
             }
             {!user && !loading && <div className="flex gap-4 items-center text-sm font-semibold">
-                <Button className={"hover:bg-white hover:text-black"} to={'signin'}>Sign In</Button>
-                <Button className={"bg-blue-500 hover:bg-blue-600"} to={'home'}>Get Started</Button>
+                <Button className={"bg-blue-500 hover:bg-blue-600"} to={'profile'}>Sign In</Button>
             </div>}
         </div>
     )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { ArrowRight, Globe, NotebookPen, Upload } from 'lucide-react';
+import axios from "axios";
+import SelectCountry from '../components/SelectCountry';
+import Container from '../components/Container';
+import Conversation from '../components/Conversation';
+
+export default function Home() {
+    const [activeTab, setActiveTab] = useState("question");
+    const [question, setQuestion] = useState("");
+    const [selectedCountry, setSelectedCountry] = useState("India");
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [showConversation, setShowConversation] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [conversation, setConversation] = useState([]);
+    const [history, setHistory] = useState([]);
+
+    const handleSubmit = async () => {
+        if (!question) {
+            alert("Please enter a question");
+            return;
+        }
+        setConversation(prev => [...prev, {role: 'user', content: question, timestamp: new Date().toLocaleTimeString()}]);
+        setLoading(true);
+        try{
+            const response = await axios.post('http://localhost:3000/api/chat', {
+                message: question
+            });
+            setConversation(prev=>[...prev, {role: 'model', content: response.data.text, timestamp: new Date().toLocaleTimeString()}]);
+            setHistory(response.data.history);
+            setShowConversation(true);
+
+        }
+        catch(error) {
+            console.error("Error fetching data:", error);
+            alert("An error occurred while fetching data. Please try again later.");
+        }
+        finally{
+            setLoading(false);
+            setQuestion("");
+        }
+    };
+
+    if(showConversation){
+        return <Conversation setQuestion={setQuestion} setShowConversation={setShowConversation} selectedCountry={selectedCountry} conversation={conversation} setConversation={setConversation} history={history} setHistory={setHistory}/>
+    }
+
+    return(
+        <div className='h-[90vh] flex justify-center items-center'>
+            <Container>
+                <div className='flex gap-2 justify-start w-full'>
+                    <Tab className={`${activeTab=="question"?"bg-green-600":"bg-gray-500 hover:bg-gray-700"}`} onClick={()=>setActiveTab("question")}><NotebookPen size={18}/> Ask Question</Tab>
+                    <Tab  className={`${activeTab=="document"?"bg-green-600":"bg-gray-500 hover:bg-gray-600"}`}><Upload size={18}/> Analyse Document</Tab>
+                </div>
+                
+                {activeTab=="question" && (
+                    <div className='w-full space-y-4'>
+                        <div className='space-y-2'>
+                            <p className='font-semibold text-sm text-gray'>What do you need help with?</p>
+                            <Textarea question={question} setQuestion={setQuestion} />
+                        </div>
+            
+                        <div className='space-y-2'>
+                            <p className='font-semibold text-sm text-gray flex items-center gap-1'><Globe size={16}/> Select your country</p>
+                            <SelectCountry showDropdown={showDropdown} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} setShowDropdown={setShowDropdown} />
+                        </div>
+                        <ActionButton loading={loading} onClick={handleSubmit}>Get Legal Analysis</ActionButton>
+                    </div>
+                )}
+            </Container>
+        </div>
+    )
+}
+
+export function Textarea({question,setQuestion, loading}){
+    const handleChange = (e) => {
+        setQuestion(e.target.value);
+    }
+    return(
+        <textarea value={question} onChange={handleChange} placeholder='e.g.  What documents are required to start a restaurant business in my country?' className={"w-full h-32 p-2 bg-primary text-white text-sm rounded-md border border-gray-600 focus:outline-none focus:ring-2 focus:ring-zinc-500 transition duration-200 resize-none disabled:pointer-events-none"} disabled={loading}/>
+    )
+}
+
+function Tab({children,onClick,className}) {
+    return (
+        <button className={`px-4 py-2 text-sm font-medium rounded-md cursor-pointer transition duration-200 ${className}`} onClick={onClick}>
+            <div className='flex gap-2'>{children}</div>
+        </button>
+    )
+}
+
+function ActionButton({children,loading, onClick}) {
+    return(
+        <button className={`flex items-center justify-center group bg-green-600 hover:bg-green-700 transition duration-200 py-2 rounded-md cursor-pointer w-full mt-4 ${loading && "cursor-not-allowed opacity-50 disabled:pointer-events-none"}`} onClick={onClick} disabled={loading}>
+            {loading ? (
+                <>
+                <div className="flex animate-spin rounded-full h-4 w-4 border-2 border-white border-t-transparent mr-2"/>Analyzing...</>
+            ) : (
+                <div className='flex gap-2 justify-center items-center'>
+                    {children}<ArrowRight size={18} className='group-hover:translate-x-1 transition-transform duration-100'/>
+                </div>
+            )}
+        </button>
+    )
+}
