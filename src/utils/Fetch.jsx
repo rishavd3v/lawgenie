185ed1bc9f629d9dab52b9c2bac32cd1244c1974
Rishav <rishavraj4273@gmail.com>
2025-06-27 01:13:29 +0530

    feat: function to create chat

---
+++ src/utils/Fetch.jsx
@@ -0,0 +1,32 @@
+import axios from "axios"
+import { useAuth } from "../context/AuthContext";
+
+export const useCreateChat = () => {
+    const { user } = useAuth();
+
+    const createChat = async (message,country,history=[]) => {
+        if (!user) {
+            throw new Error("User not authenticated. Please sign in to continue.");
+        }
+        try{
+            const token = await user.getIdToken();
+            const res = await axios.post(
+                'http://localhost:3000/api/chat',
+                { message, country, history },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            return res.data;
+        }
+        catch(error) {
+            console.error("Error creating chat:", error);
+            throw new Error("An error occurred while creating the chat. Please try again later.");
+        }
+
+    };
+
+    return createChat;
+};
