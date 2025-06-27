d40013d95e96709257b78cda7a3a9fd32d6b3ad9
Rishav <rishavraj4273@gmail.com>
2025-06-27 23:13:03 +0530

    chore: update axios params

--- src/utils/Fetch.jsx
+++ src/utils/Fetch.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../context/AuthContext";
 export const useCreateChat = () => {
     const { user } = useAuth();
 
-    const createChat = async (message,country,history=[]) => {
+    const createChat = async (message,country,history=[], conversationId) => {
         if (!user) {
             throw new Error("User not authenticated. Please sign in to continue.");
         }
@@ -12,7 +12,7 @@ export const useCreateChat = () => {
             const token = await user.getIdToken();
             const res = await axios.post(
                 'http://localhost:3000/api/chat',
-                { message, country, history },
+                { message, country, history, conversationId },
                 {
                     headers: {
                         Authorization: `Bearer ${token}`,
