5a7c73c19c0e3031e03dfb124627e8a023f00db5
Rishav <rishavraj4273@gmail.com>
2025-07-29 00:18:08 +0530

    chore: refractor

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,20 @@ import Navbar from './components/Navbar'
 import SignIn from './pages/SignIn'
 import Profile from './pages/Profile'
 import ProtectedRoute from './components/Auth/ProtectedRoute'
+import Conversation from './components/Conversation'
+import ChatPage from './pages/ChatPage'
 
 function App() {
 
   return (
     <Router>
-      <Navbar/>
-      <div className='px-30'>
         <Routes>
           <Route path="/" element={<Landing/>}/>
-          <Route path="/home" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
           <Route path="/signin" element={<SignIn/>}/>
           <Route path="/profile" element={<ProtectedRoute><Profile/></ProtectedRoute>}/>
+          <Route path="/home" element={<ProtectedRoute><Home/></ProtectedRoute>}/>
+          <Route path="/chat/:conversationId" element={<ProtectedRoute><ChatPage/></ProtectedRoute>}/>
         </Routes>
-      </div>
     </Router>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,55 @@
-import { useState } from 'react';
-import { ArrowRight, Globe, NotebookPen, Upload } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { ArrowRight, Globe, Menu, NotebookPen, Upload } from 'lucide-react';
 import SelectCountry from '../components/SelectCountry';
 import Container from '../components/Container';
-import Conversation from '../components/Conversation';
 import { useCreateChat } from '../utils/fetch';
+import Grid from '../components/ui/Grid';
+import Sidebar from '../components/Sidebar';
+import { useChatStore } from '../store/chatStore';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+    const {question,setQuestion, selectedCountry, setSelectedCountry,setHistory,setConversation,setConversationId, addMessage} = useChatStore();
     const [activeTab, setActiveTab] = useState("question");
-    const [question, setQuestion] = useState("");
-    const [selectedCountry, setSelectedCountry] = useState("India");
     const [showDropdown, setShowDropdown] = useState(false);
-    const [showConversation, setShowConversation] = useState(false);
     const [loading, setLoading] = useState(false);
-    const [conversation, setConversation] = useState([]);
-    const [history, setHistory] = useState([]);
-    const [conversationId, setConversationId] = useState(null);
     const createChat = useCreateChat();
+    const navigate = useNavigate();
+    const [showSidebar, setShowSidebar] = useState(true);
+    const [sidebarClosed, setSidebarClosed] = useState(false);
+
+    useEffect(() => {
+        if(!showSidebar){
+            const timeout = setTimeout(() => setSidebarClosed(true), 300);
+            return () => clearTimeout(timeout);
+        }
+        else{
+            setSidebarClosed(false);
+        }
+    }, [showSidebar]);
 
     const handleSubmit = async () => {
         if (!question) {
             alert("Please enter a question");
             return;
         }
-        setConversation(prev => [...prev, {role: 'user', content: question, timestamp: new Date().toLocaleTimeString()}]);
+        setConversation(question.slice(0,15));
+        addMessage({
+            role: 'user',
+            content: question,
+            timestamp: new Date().toLocaleTimeString(),
+        });
         setLoading(true);
         try{
             const response = await createChat(question,selectedCountry);
-            setConversation(prev=>[...prev, {role: 'model', content: response.text, timestamp: new Date().toLocaleTimeString()}]);
+            addMessage({
+                role: 'model',
+                content: response.text,
+                timestamp: new Date().toLocaleTimeString(),
+            });
             setConversationId(response.conversationId);
             setHistory(response.history);
-            setShowConversation(true);
-
+            navigate(`/chat/${response.conversationId}`);
         }
         catch(error) {
             setQuestion("");
@@ -43,34 +62,40 @@ export default function Home() {
         }
     };
 
-    if(showConversation){
-        return <Conversation setQuestion={setQuestion} setShowConversation={setShowConversation} selectedCountry={selectedCountry} conversation={conversation} setConversation={setConversation} history={history} setHistory={setHistory} conversationId={conversationId} setConversationId={setConversationId}/>
-    }
-
     return(
-        <div className='h-[90vh] flex justify-center items-center'>
-            <Container>
-                <div className='flex gap-2 justify-start w-full'>
-                    <Tab className={`${activeTab=="question"?"bg-green-600":"bg-gray-500 hover:bg-gray-700"}`} onClick={()=>setActiveTab("question")}><NotebookPen size={18}/> Ask Question</Tab>
-                    <Tab disabled={true}  className={`${activeTab=="document"?"bg-green-600":"bg-gray-500 hover:bg-gray-600"}`}><Upload size={18}/> Analyse Document</Tab>
-                </div>
-                
-                {activeTab=="question" && (
-                    <div className='w-full space-y-4'>
-                        <div className='space-y-2'>
-                            <p className='font-semibold text-sm text-gray'>What do you need help with?</p>
-                            <Textarea question={question} setQuestion={setQuestion} />
-                        </div>
-            
-                        <div className='space-y-2'>
-                            <p className='font-semibold text-sm text-gray flex items-center gap-1'><Globe size={16}/> Select your country</p>
-                            <SelectCountry showDropdown={showDropdown} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} setShowDropdown={setShowDropdown} />
-                        </div>
-                        <ActionButton loading={loading} onClick={handleSubmit}>Get Legal Analysis</ActionButton>
+        <Grid>
+            <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar}/>
+
+            {sidebarClosed && (
+                <button className="fixed top-4 left-4 z-50 bg-gray-800 text-white p-2 rounded-md shadow hover:bg-gray-700 transition-all cursor-pointer" onClick={() => setShowSidebar(true)}>
+                    <Menu size={20} />
+                </button>
+            )}
+
+            <div className={`col-span-5 h-screen flex justify-center items-center transition-transform duration-300 ease-in-out ${!showSidebar && "-translate-x-30"}`}>
+                <Container className='w-3/4'>
+                    <div className='flex gap-2 justify-start'>
+                        <Tab className={`${activeTab=="question"?"bg-green-600":"bg-gray-500 hover:bg-gray-700"}`} onClick={()=>setActiveTab("question")}><NotebookPen size={18}/> Ask Question</Tab>
+                        <Tab disabled={true}  className={`${activeTab=="document"?"bg-green-600":"bg-gray-500 hover:bg-gray-600"}`}><Upload size={18}/> Analyse Document</Tab>
                     </div>
-                )}
-            </Container>
-        </div>
+                    
+                    {activeTab=="question" && (
+                        <div className='w-full space-y-4'>
+                            <div className='space-y-2'>
+                                <p className='font-semibold text-sm text-gray'>What do you need help with?</p>
+                                <Textarea question={question} setQuestion={setQuestion} />
+                            </div>
+                
+                            <div className='space-y-2'>
+                                <p className='font-semibold text-sm text-gray flex items-center gap-1'><Globe size={16}/> Select your country</p>
+                                <SelectCountry showDropdown={showDropdown} selectedCountry={selectedCountry} setSelectedCountry={setSelectedCountry} setShowDropdown={setShowDropdown} />
+                            </div>
+                            <ActionButton loading={loading} onClick={handleSubmit}>Get Legal Analysis</ActionButton>
+                        </div>
+                    )}
+                </Container>
+            </div>
+        </Grid>
     )
 }
 
