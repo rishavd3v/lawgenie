4d6d5b3ed17bf039cc203511c3d15c67bfe1ebe9
Rishav <rishavraj4273@gmail.com>
2025-07-29 00:19:13 +0530

    chore: refractor

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -4,7 +4,6 @@ import LoadingSpinner from "../LoadingSpinner";
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
-
   if (loading) return <LoadingSpinner/>;
   if (!user) return <Navigate to="/signin" />;
 
