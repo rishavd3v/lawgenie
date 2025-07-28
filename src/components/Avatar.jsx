5cea3a26dd1e9d2465522fbec3b51cd20464848f
Rishav <rishavraj4273@gmail.com>
2025-07-29 00:14:46 +0530

    feat: onCLick handler

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,11 +1,10 @@
 import { User } from 'lucide-react';
-import profileSvg from '../assets/profile.svg';
 import { useAuth } from '../context/AuthContext';
-export default function Avatar({ref,size=10}){
+export default function Avatar({ref,size=10, onClick}){
     const {user} = useAuth();
     return(
-        <div ref={ref}>
-            {user && user.photoURL ? <img src={user.photoURL} alt="" className={`aspect-square rounded-full w-${size}`}/>:<div className='rounded-full aspect-square flex items-center justify-center bg-gray-600 p-2'><User size={20}/></div>}            
+        <div ref={ref} onClick={onClick}>
+            {user && user.photoURL ? <img src={user.photoURL} alt="" className={`cursor-pointer aspect-square rounded-full w-${size}`}/>:<div className='rounded-full aspect-square flex items-center justify-center bg-gray-600 p-2'><User size={20}/></div>}            
         </div>
     )
 }
